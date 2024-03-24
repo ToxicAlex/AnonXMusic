5195e6f250501c76d60209ea84aafe35b7d0990a
𝐃𝛆ᴠɪ֟፝‌꯭ʟ𝁂꯭꯭꯭֯ ᴋɪ꯭ɴʛ𝆺꯭𝅥༎ࠫ <136243912+itsdevxd@users.noreply.github.com>
2024-03-24 15:06:57 +0530

    Update and rename start (2).py to start .py
    Signed-off-by: 𝐃𝛆ᴠɪ֟፝‌꯭ʟ𝁂꯭꯭꯭֯ ᴋɪ꯭ɴʛ𝆺꯭𝅥༎ࠫ <136243912+itsdevxd@users.noreply.github.com>

--- AnonXMusic/utils/inline/start .py
+++ AnonXMusic/utils/inline/start .py
@@ -26,7 +26,7 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url="https://t.me/thefriends_club"),
+            InlineKeyboardButton(text=_["S_B_2"], url="https://t.me/TheFriendsHome"),
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
         [
