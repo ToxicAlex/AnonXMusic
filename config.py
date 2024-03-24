54f82a87553051c6551f6e69aa7a9b1f7adcfc6c
𝐃𝛆ᴠɪ֟፝‌꯭ʟ𝁂꯭꯭꯭֯ ᴋɪ꯭ɴʛ𝆺꯭𝅥༎ࠫ <136243912+itsdevxd@users.noreply.github.com>
2024-03-24 16:17:21 +0530

    Update config.py
    Signed-off-by: 𝐃𝛆ᴠɪ֟፝‌꯭ʟ𝁂꯭꯭꯭֯ ᴋɪ꯭ɴʛ𝆺꯭𝅥༎ࠫ <136243912+itsdevxd@users.noreply.github.com>

--- config.py
+++ config.py
@@ -81,7 +81,7 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://te.legra.ph/file/169fcc1d286270b50f4fd.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/2cd51c02ca022e6162ea7.jpg"
+    "PING_IMG_URL", "https://te.legra.ph/file/7c06cfce1425c4fa6cf86.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
