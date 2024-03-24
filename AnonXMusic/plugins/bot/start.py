4efd12136e50ca948aa8d4e890a7e1d2e26962a0
𝐃𝛆ᴠɪ֟፝‌꯭ʟ𝁂꯭꯭꯭֯ ᴋɪ꯭ɴʛ𝆺꯭𝅥༎ࠫ <136243912+itsdevxd@users.noreply.github.com>
2024-03-24 15:16:18 +0530

    Update start.py
    Signed-off-by: 𝐃𝛆ᴠɪ֟፝‌꯭ʟ𝁂꯭꯭꯭֯ ᴋɪ꯭ɴʛ𝆺꯭𝅥༎ࠫ <136243912+itsdevxd@users.noreply.github.com>

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -85,7 +85,7 @@ async def start_pm(client, message: Message, _):
     else:
         out = private_panel(_)
             await message.reply_sticker(
-                Id=(
+                Id=("CAACAgUAAx0EeHjNzQACC-1l__YTLJhWFDQwP9wIjTSHERXGXgACjwQAAjRU0VW4kdsdISdvmh4E")
             await message.reply_photo(
               photo="https://te.legra.ph/file/169fcc1d286270b50f4fd.jpg",
               caption=_["start_2"].format(message.from_user.mention, app.mention),
